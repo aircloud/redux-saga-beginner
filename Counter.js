380484a663f3e90de45481ee0fa1f33b4e899bfa
iconie(聂小涛) <iconie@tencent.com>
2017-09-11 17:25:23 +0800

    init the porgram

---
+++ Counter.js
@@ -0,0 +1,28 @@
+/*eslint-disable no-unused-vars */
+import React, { Component, PropTypes } from 'react'
+
+const Counter = ({ value, onIncrement, onDecrement, onIncrementAsync  }) =>
+      <div>
+        <button onClick={onIncrement}>
+          Increment
+        </button>
+        {' '}
+        <button onClick={onDecrement}>
+          Decrement
+        </button>
+        <button onClick={onIncrementAsync}>
+            Increment after 1 second
+        </button>
+        <hr />
+        <div>
+          Clicked: {value} times
+        </div>
+      </div>
+
+Counter.propTypes = {
+  value: PropTypes.number.isRequired,
+  onIncrement: PropTypes.func.isRequired,
+  onDecrement: PropTypes.func.isRequired
+};
+
+export default Counter
---
+++ README.md
@@ -0,0 +1,25 @@
+>这是一个应用redux-saga的小demo，可以结合[这里](http://leonshi.com/redux-saga-in-chinese/docs/introduction/BeginnerTutorial.html)进行学习
+
+# redux-saga-beginner
+
+It is a demo for redux-saga, just for beginners to learning.
+
+# Instructions
+
+Setup
+
+```
+// clone the repo
+git clone https://github.com/redux-saga/redux-saga-beginner-tutorial.git
+
+cd redux-saga-beginner-tutorial
+
+npm install
+```
+
+Run the demo
+
+```
+npm start
+```
+
